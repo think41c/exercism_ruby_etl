5690a82c8e8260b99c6c8cf8e570e180f8b05a62
Tony DiNitto <think41c@gmail.com>
2015-04-21 22:03:01 -0400

    Cleaned up code / unncessary lines.

--- etl.rb
+++ etl.rb
@@ -2,26 +2,15 @@ class ETL
   def self.transform(old)
     newer_hash = {}
     new_hash = old.invert 
-    # puts old
-    # puts new_hash
-    # Invert the hash, downcase the key. 
-    # Most likely use a Hash.map function.
     new_hash.map do |x, y|
       a = p x.join.downcase
-      # p a
       newer_hash[a] = y
-      # p newer_hash
-      # [x.capitalize, y]
-      # p newer_hash
     end
-    # p newer_hash
     newer_hash
   end
 
 
 end
 
-# Code snippet to consider - 
-# countries = Hash[countries.map {|country, abbr| [country.capitalize, abbr] }]
 old = { 1 => ['A'] , 2 => ["B"]}
 ETL.transform(old)
